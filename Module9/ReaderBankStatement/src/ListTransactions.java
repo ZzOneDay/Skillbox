41fb82ec03bbd5a71074509a63da37b5c512c250
ZzOneDay <azminseo@gmail.com>
2019-12-12 18:59:07 +0300

    Update

--- Module9/ReaderBankStatement/src/ListTransactions.java
+++ Module9/ReaderBankStatement/src/ListTransactions.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class ListTransactions
-{
+public class ListTransactions {
     private ArrayList<Transaction> transactions;
 
 
@@ -9,7 +13,7 @@ public class ListTransactions
         this.transactions = new ArrayList<>(transactions);
     }
 
-    public Double getFullReceipt(){
+    public Double getFullReceipt() {
         Double receipt = 0.0;
         for (Transaction transaction : transactions) {
             receipt += transaction.getReceipt();
@@ -17,7 +21,7 @@ public class ListTransactions
         return receipt;
     }
 
-    public Double getFullExpense(){
+    public Double getFullExpense() {
         Double expanse = 0.0;
         for (Transaction transaction : transactions) {
             expanse += transaction.getExpense();
@@ -25,13 +29,52 @@ public class ListTransactions
         return expanse;
     }
 
-    public void sortTransactionsByDate (boolean inOrder) {
+    public static ListTransactions getNewListTransactionsSortByDate(ListTransactions ArrayTransactions, boolean inOrder) {
+        ArrayList<Transaction> transactions = new ArrayList<>(ArrayTransactions.getTransactions());
         if (inOrder) {
             transactions.sort((o1, o2) -> o1.getDateOfTransaction().compareTo(o2.getDateOfTransaction()));
-        }
-        else {
+        } else {
             transactions.sort((o1, o2) -> o2.getDateOfTransaction().compareTo(o1.getDateOfTransaction()));
         }
+        return new ListTransactions(transactions);
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public static HashMap<String, ListTransactions> getNewListTransactionsSortedByGroup(ListTransactions listTransactions) {
+        HashMap<String, ArrayList<Transaction>> mapArrayTransactionSortedByGroup = new HashMap<>();
+        ArrayList<Transaction> transactions = listTransactions.getTransactions();
+        for (Transaction transaction : transactions) {
+            if (!mapArrayTransactionSortedByGroup.containsKey(transaction.getDescriptionOfTransaction())) {
+                //не находит
+                ArrayList<Transaction> newList = new ArrayList<>();
+                newList.add(transaction);
+                mapArrayTransactionSortedByGroup.put(transaction.getDescriptionOfTransaction(), newList);
+            } else {
+                //находит
+                mapArrayTransactionSortedByGroup.get(transaction.getDescriptionOfTransaction()).add(transaction);
+            }
+        }
+        return getNewTransactionsMap(mapArrayTransactionSortedByGroup);
+    }
+
+    private static HashMap<String, ListTransactions> getNewTransactionsMap(HashMap<String, ArrayList<Transaction>> mapTransactions) {
+        HashMap<String, ListTransactions> listTransactionsSortedByGroup = new HashMap<>();
+        Set<String> keys = mapTransactions.keySet();
+        for (String key : keys) {
+            ListTransactions listTransactions = new ListTransactions(mapTransactions.get(key));
+            listTransactionsSortedByGroup.put(key, listTransactions);
+        }
+        return listTransactionsSortedByGroup;
+    }
+
+    public static String getCorrectNameGroup(String information) {
+        Pattern r = Pattern.compile("//.+\\t]");
+        Matcher m = r.matcher(information);
+        String needToCorrectPart = information.substring(m.start() + 1, m.end() - 1).replaceAll(",", ".");
+        return needToCorrectPart;
     }
 
     public static void printList (ListTransactions listTransactions) {
--- Module9/ReaderBankStatement/src/Main.java
+++ Module9/ReaderBankStatement/src/Main.java
@@ -1,23 +1,36 @@
-import java.util.ArrayList;
+import java.util.*;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        ArrayList<Transaction> transactions = new ArrayList<>();
-        ReaderTransactionList readerTransactionList = new ReaderTransactionList();
-        String path = "C:\\Users\\Admin\\Desktop\\Skillbox\\Module9\\ReaderBankStatement\\res\\movementList.csv";
-        ArrayList<String[]> arrayList = readerTransactionList.getInformation(path);
-        System.out.println("size" + arrayList.size());
+        String information = "548673++++++1028    809216  /RU/CARD2CARD ALFA_MOBILE>MOSCOW          23.04.17 23.04.17 1000.00       RUR MCC6536";
+        System.out.println(ListTransactions.getCorrectNameGroup(information));
+        //нужно дописать метод, чтобы правильно формировались ключи
 
-        for (String[] arrayTransaction : arrayList) {
-            transactions.add(new Transaction(arrayTransaction));
-        };
-
-        ListTransactions listTransactions = new ListTransactions(transactions);
-        System.out.println(listTransactions.getFullReceipt());
-        System.out.println(listTransactions.getFullExpense());
-        ListTransactions.printList(listTransactions);
-        listTransactions.sortTransactionsByDate(false);
-        ListTransactions.printList(listTransactions);
     }
 }
+//        ArrayList<Transaction> transactions = new ArrayList<>();
+//        ReaderTransactionList readerTransactionList = new ReaderTransactionList();
+//        String path = "res/movementList.csv";
+//        ArrayList<String[]> arrayList = readerTransactionList.getInformation(path);
+//        System.out.println("size" + arrayList.size());
+//
+//        for (String[] arrayTransaction : arrayList) {
+//            transactions.add(new Transaction(arrayTransaction));
+//        };
+//        ListTransactions listTransactions = new ListTransactions(transactions);
+////        System.out.println(listTransactions.getFullReceipt());
+////        System.out.println(listTransactions.getFullExpense());
+////        ListTransactions.printList(listTransactions);
+////        ListTransactions sorted = ListTransactions.getNewListTransactionsSortByDate(listTransactions, true);
+////        ListTransactions.printList(sorted);
+//
+//
+//        HashMap<String, ListTransactions> transactionsSortedByGroup = ListTransactions.getNewListTransactionsSortedByGroup(listTransactions);
+//        Set<String> groupName = transactionsSortedByGroup.keySet();
+//        for (String oneGroup : groupName) {
+//            ListTransactions list = transactionsSortedByGroup.get(oneGroup);
+//            System.out.println(list.getTransactions().get(0).getDescriptionOfTransaction() + " Full Expensive " +
+//                    + list.getFullExpense() + " Full Receipt " + list.getFullReceipt() ) ;
+//        }
+//    }
+//}
